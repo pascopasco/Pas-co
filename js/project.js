00acfbaf6bf0c0d96bc2981d09ce58e5e79dbc9c
LopezN26 <n.lopez@it-students.fr>
2020-04-27 21:42:55 +0200

    page project finie

--- js/project.js
+++ js/project.js
@@ -5,6 +5,24 @@ function generatePage() {
     $("#descContent").append(data.projects[selectedProject].titre);
     $("#video").append(data.projects[selectedProject].lien_video);
     $("#text").append(data.projects[selectedProject].description);
+    if (data.projects[selectedProject].materiel.length>0)
+    {
+        $("#material").append("<h4>Matériel</h4>");
+        for (i=0;i<data.projects[selectedProject].materiel.length;i++)
+        {
+            $("#material").append((`<p><span>—</span>${data.projects[selectedProject].materiel[i]}</p>`));
+        }
+    }
+    if (data.projects[selectedProject].equipe.length>0)
+    {
+        $("#team").append("<h4>Équipe</h4>");
+        for (i=0;i<data.projects[selectedProject].equipe.length;i++)
+        {
+            $("#team").append((`<p><span>—</span>${data.projects[selectedProject].equipe[i]}</p>`));
+        }
+    }
+
+
 }
 
 generatePage();
