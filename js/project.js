6d9ca1c1cf5343a3c777cd26dc6196ec1f86f589
LopezN26 <n.lopez@it-students.fr>
2020-05-26 16:06:32 +0200

    review 1 en cours

--- js/project.js
+++ js/project.js
@@ -2,12 +2,12 @@ function generatePage() {
     let selectedProject = localStorage.getItem("selectedProject");
     $("#test").append(`<p> ${data.projects[selectedProject].description}</p>`);
     $("#pageName").append(data.projects[selectedProject].sousTitre);
-    $("#descContent").append(data.projects[selectedProject].titre);
+    $("#descContent").append(`<p>${data.projects[selectedProject].titre}</p>`);
     $("#video").append(data.projects[selectedProject].lien_video);
     $("#text").append(data.projects[selectedProject].description);
     if (data.projects[selectedProject].materiel.length>0)
     {
-        $("#material").append("<h4>Matériel</h4>");
+        $("#material").append("<h4>MATÉRIEL</h4>");
         for (i=0;i<data.projects[selectedProject].materiel.length;i++)
         {
             $("#material").append((`<p><span>—</span>${data.projects[selectedProject].materiel[i]}</p>`));
@@ -15,7 +15,7 @@ function generatePage() {
     }
     if (data.projects[selectedProject].equipe.length>0)
     {
-        $("#team").append("<h4>Équipe</h4>");
+        $("#team").append("<h4>ÉQUIPE</h4>");
         for (i=0;i<data.projects[selectedProject].equipe.length;i++)
         {
             $("#team").append((`<p><span>—</span>${data.projects[selectedProject].equipe[i]}</p>`));
