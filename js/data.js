48813880add274c4fee12249317ef689396ffc1d
LopezN26 <n.lopez@it-students.fr>
2020-05-26 20:20:35 +0200

    changement des images portfolio

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ let data = {
                 {
                     "titre": "Soirée crêpes ",
                     "sousTitre": "Clip Vidéo",
-                    "image": `./img/icones_videos/Crepes.jpg`,
+                    "image": `./img/icones_videos/crepes.jpg`,
                     "description" : "Cette production est le résultat d'un défi technique : filmer caméra portée avec uniquement des mouvement fait à la main. Le but était dans un premier temps d’offrir une immersion au cœur de l’action de ce qui se passe à travers cette « soirée crêpes » que s’offre quelques amis. Mais le projet s’est vu évoluer vers une volonté de capturer plus simplement une esthétique et une poésie de l’image. J'avais vraiment l'envie de mettre en avant la qualité visuelle, la beauté de l'image. Une ambiance qui passe du chaleureux avec la bougie et qui évolue vers un inquiétante étrangeté de ces personnages marqués par un sourire angoissant. Un projet que j'ai écrit, réalisé, filmé et monté. J'ai eu la chance de travailler Gabriel CAMPOS, un compositeur qui a construit une superbe musique pour mon projet.",
                     "materiel" : ["Panasonic Lumix GH5", "Objectif Sigma 18-35mm F1.8", "Éclairage LED Aputure LS C120D", "Aputure Light Dome II"],
                     "lien_video" : `<iframe src="https://www.youtube.com/embed/tLEnjXspiLw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
@@ -19,7 +19,7 @@ let data = {
                 },{
                     "titre": "Festival du cinéma OJO LOCO",
                     "sousTitre": "Aftermovie",
-                    "image": `./img/icones_videos/OJO.jpg`,
+                    "image": `./img/icones_videos/ojo_loco.jpg`,
                     "description" : "J'ai eu la chance d'être stagiaire dans la 7ème édition du festival du cinéma ibérique et latino-américain. J'ai réalisé plusieurs vidéos visant à promouvoir le festival dont cet Aftermovie. Un projet dans lequel j’étais très libre ce qui m’a amené à monter le projet en gardant un esprit humoristique mettant en scène mon assistant caméra.",
                     "materiel" : ["Canon EOS 77D"],
                     "lien_video" : `<iframe src="https://www.youtube.com/embed/81wNqardq08" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
@@ -27,7 +27,7 @@ let data = {
                 },{
                     "titre": "Pain et Compagnie",
                     "sousTitre": "Projet Esthétique",
-                    "image": `./img/icones_videos/Pain_et_compagnie.jpg`,
+                    "image": `./img/icones_videos/pain_et_compagnie.jpg`,
                     "description" : "Pain et compagnie est un projet qui part d’une envie soudaine de prendre des images d’un instant que j’ai voulu immortaliser : la sortie du four de pain au levain fait maison. C’était aussi l’occasion de d’exploiter le potentiel du 180 FPS du GH5. Venez déguster du bon pain frais avec sa noisette de beurre et sa pincée de sel !",
                     "materiel" : ["Panasonic Lumix GH5", "Objectif Hybride Panasonic Lumix G Vario 12-60 mm f/3.5-5.6"],
                     "lien_video" : `<iframe src="https://www.youtube.com/embed/8RWPNxNajME" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
@@ -35,7 +35,7 @@ let data = {
                 },{
                     "titre": "Interphobe",
                     "sousTitre": "72h du Cinéma",
-                    "image": `./img/icones_videos/Interphobe.jpg`,
+                    "image": `./img/icones_videos/interphobe.jpg`,
                     "description" : "Voici un huis-clos réalisé dans le cadre du 72h du Cinéma du Téléthon 2019. Le but étant de réaliser un court métrage de maximum 3 minutes dans lequel on sensibilise les gens au handicap en général. Tout le propos est basé sur le dialogue qui s’opère entre deux passants qui se retrouve dans un SAS. L’un aimerait rentrer dans le bâtiment mais il semble incapable de toucher l’interphone. Incapable ?",
                     "materiel" : ["Sony PXW-FS7 II, Objectif Rokinon 85mm T1.5", "Éclairage LED Aputure LS C120D II, Reflecteur/Diffuseur MultiDisc", "Aputure Light Dome 2", "Enregistreur audio Zoom H6", "Microphone Sennheiser ME66-K6 + Zeppelin", "Perche Audio pour Shotgun"],
                     "lien_video" : `<iframe src="https://www.youtube.com/embed/TaKiSWrqE1Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
@@ -43,7 +43,7 @@ let data = {
                 },{
                     "titre": "J’ai viré – Deuzel ",
                     "sousTitre": "Clip vidéo",
-                    "image": `./img/icones_videos/Jai_vire.jpg`,
+                    "image": `./img/icones_videos/jai_vire.jpg`,
                     "description" : "Voici mon premier clip vidéo réalisé pour le groupe Deuzel. Un projet qui m'a donné beaucoup d'espoir car le résultat fut très satisfaisant pour une première. Une danseuse professionnelle s’est offerte à l’objectif par la danse à merveille. Une première qui m’a donné goût au cadrage en action. La danse est un art que j’apprécie à filmer car il incite à tenter des mouvements de caméras très satisfaisants.",
                     "materiel" : ["Panasonic Lumix GH5", "Objectif Hybride Panasonic Lumix G Vario 12-60 mm f/3.5-5.6", "RONIN S"],
                     "lien_video" : `<iframe src="https://www.youtube.com/embed/E32ygX_B2dY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
@@ -51,7 +51,7 @@ let data = {
                 },{
                     "titre": "Apocalypse Snow",
                     "sousTitre": "Bande annonce",
-                    "image": `./img/icones_videos/Apocalypse_Snow.jpg`,
+                    "image": `./img/icones_videos/apocalypse_snow.jpg`,
                     "description" : "Apocalypse Snow est une bande annonce d'un ''western dans la neige''. Si le froid canadien a sûrement été la plus grosse difficulté pendant le tournage (-30° au quotidien), le plaisir de s’aventurer en équipe réduite sur un immense lac gelé, dans une forêt fraîchement recouverte de quelques mètres de neige, de faire un feu sur la neige de nuit et finalement de côtoyer la flore unique des paysages nordique de cet immense pays ont marqué au fer rouge des souvenirs de tournages fantastiques… Les costumes étant très légers, les acteurs et l'actrice ont fait beaucoup d'efforts et le planning de tournage a coûte que coûte été respecté. Le montage qui vous est présenté ci-dessus est une première version longue de la bande annonce. Le projet est à raccourcir et à retravailler en termes de design sonore et de montage mais les images sont là. J’ai eu la chance de cadrer les 90% du projet à main nue sur la merveilleuse Canon C200 !",
                     "materiel" : ["Camescope Sony PXW-FS7", "Objectif Rokinon 35mm T1.5", "Objectif Rokinon 85mm T1.5", "Éclairage LED Aputure C120D II", "Éclairage LED Amaran", "Trépied Manfrotto Combi-boom 420B", "Aputure Light Dome 2", "Ecran SmallHD", "Microphone omnidirectionnel Sennheiser ME62-K6", "Enregistreur audio Zoom H6"],
                     "lien_video" : `<iframe src="https://www.youtube.com/embed/L0fqMj0kHnE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
@@ -60,7 +60,7 @@ let data = {
                {
                    "titre": "Déréliction",
                    "sousTitre": "Clip vidéo",
-                   "image": `./img/icones_videos/Dere2.jpg`,
+                   "image": `./img/icones_videos/dereliction.jpg`,
                    "description" : "Déréliction est un Clip vidéo dans lequel est mis en scène une fille chamboulée par son quotidien dans lequel elle s'isole malgré elle. Ce « sentiment d’abandon et de solitude morale » - tel le défini le titre du projet - que vit la protagoniste met le projecteur sur ces zones d’ombres si importantes que beaucoup de personnes intériorisent. Le projet tente de s’inclure dans le mouvement de la postmodernité et utilise des procédés de la métalepse narrative. \n" +
                        "Encore une fois, on retiendra plus les expériences uniques de contextes de tournages comme les lames de couteaux du vent arctique glacial au bord du lac qui viennent fouetter nos visages et une neige abondante apportant un décor absolument extraordinaire. Des ambiances féériques et fabuleuses qui permettent de rajouter de la magie à ces projets très formateurs durant cette année canadienne.",
                    "materiel" : ["Camescope Canon C200","Stabilisateur Glidecam X-22 (avec veste et bras)", "Ecran SmallHD", "Caméra Canon 6D Mark II", "Objectif Sigma 18-35mm F1.8", "Objectif Canon EF 100mm f/2.8L Macro IS USM", "Éclairage LED Aputure LS C120D II", "Éclairage LED Amaran", "Aputure Light Dome 2", "Microphone omnidirectionnel Sennheiser ME62-K6", "Enregistreur audio Zoom H4n et Zoom H6", "Panasonic Lumix GH5","Objectif Hybride Panasonic Lumix G Vario 12-60 mm f/3.5-5.6"],
@@ -70,7 +70,7 @@ let data = {
                 {
                     "titre": "Combustion",
                     "sousTitre": "Court-métrage",
-                    "image": `./img/icones_videos/Combustion.jpg`,
+                    "image": `./img/icones_videos/combustion.jpg`,
                     "description" : "Combustion est un projet réalisé en 48h dans le cadre du KINO, une expérience cinématographique que met en place un professeur lors de son temps libre chaque année. C’est mon premier projet en noir et blanc que l’on a donc écris, tourné et monté dans le rush en 48h. Ces projets réalisé en quelques jours et parfois un seul sont des exercices très formateurs car ils apprennent à travailler en équipe l’efficacité, l’organisation en obligeant à donner le maximum pendant un temps limité. Ce sont aussi des expérience de vie car on reste ensemble sans arrêt durant ces week-end de rushes. Très plaisant et gratifiant, j’ai hâte d’en refaire. En attendant voici deux personnes qui font un pari sur un homme d’apparence très mystérieuse…",
                     "materiel" : ["Camescope Canon C200", "Objectif Rokinon 35mm T1.5", "Rokinon 85mm T1.5", "Objectif Canon EF 70-200mm f/2.8 IS II USM", "Ecran SmallHD"],
                     "lien_video" : `<iframe src="https://www.youtube.com/embed/UseFh5uRpTI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
@@ -78,7 +78,7 @@ let data = {
                 },{
                     "titre": "Soirée Déréliction ",
                     "sousTitre": "Clip vidéo",
-                    "image": `./img/icones_videos/Dere1.jpg`,
+                    "image": `./img/icones_videos/dereliction_soiree.jpg`,
                     "description" : "Je ne voulais pas laisser mourir des images que j'appréciais d'un tournage servant le Clip de Déréliction (on peut mettre le lien ?) tourné en décembre 2019. J'ai donc repris les rushes que j’avais tourné et en ait fait un montage basé sur la même protagoniste de Déréliction. J’ai travaillé le montage afin de changer l’ambiance de précèdent projet. On peut le voir comme une suite, avec ce personnage principal qui évolue de manière inquiétante, comme touché par une magie noire. Libre à vous de saisir le propos de l’histoire. Le personnage devient une sorte de malfaisante shamanique qui ne restera pas les bras croisés pendant cette soirée étrange dans laquelle se sont embarqué les invités insouciants…",
                     "materiel" : ["Camescope Canon C200", "Objectif Canon EF 100mm f/2.8L Macro IS USM", "Éclairage LED Aputure LS C120D II", "Aputure Light Dome 2"],
                     "lien_video" : `<iframe src="https://www.youtube.com/embed/q0oXeWyd3DA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
